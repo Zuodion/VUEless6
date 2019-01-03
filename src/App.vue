7154e248cf9fb8957334f0fcfb537f2bed2e9501
Ivanov Oleksandr <ivanov.oleksandr@intenselab.com>
2019-01-03 16:00:17 +0200

    Animation in JS and CSS

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,207 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br>
+                <br>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br>
+                <br>
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-if="show">This is some info1</div>
+                </transition>
+                <transition name="slide" type="animation" appear>
+                    <div class="alert alert-info" v-if="show">This is some info2</div>
+                </transition>
+                <transition
+                    enter-active-class="animated rotateIn"
+                    leave-active-class="animated rotateOut"
+                >
+                    <div class="alert alert-info" v-if="show">This is some info3</div>
+                </transition>
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some info4</div>
+                    <div class="alert alert-warning" v-else key="warning">This is some warning4</div>
+                </transition>
+                <hr>
+                <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+                <br>
+                <br>
+                <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled"
+                    :css="false"
+                >
+                    <div
+                        style="width: 300px; height:100px; background-color: lightgreen"
+                        v-if="load"
+                    ></div>
+                </transition>
+                <hr>
+                <button
+                    class="btn btn-primary"
+                    @click="selectedComponent === 'success-alert' ? selectedComponent = 'danger-alert' : selectedComponent = 'success-alert'"
+                >Toggle Components</button>
+                <br>
+                <br>
+                <transition name="fade" mode="out-in">
+                    <component :is="selectedComponent"></component>
+                </transition>
+                <hr>
+                <button class="btn btn-primary" @click="addItem">Add Item</button>
+                <br>
+                <br>
+                <ul class="list-group">
+                    <transition-group name="slide">
+                        <li
+                            class="list-group-item"
+                            v-for="(number, index) in numbers"
+                            :key="number"
+                            @click="removeItem(index)"
+                            style="cursor: pointer"
+                        >{{ number }}</li>
+                    </transition-group>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DangerAlert from "./DangerAlert.vue";
+import SuccessAlert from "./SuccessAlert.vue";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            show: false,
+            load: true,
+            alertAnimation: "fade",
+            elementWidth: 100,
+            selectedComponent: "success-alert",
+            numbers: [1, 2, 3, 4, 5]
+        };
+    },
+    methods: {
+        addItem() {
+            const newNumber = Math.floor(this.numbers.length);
+            this.numbers.splice(newNumber, 0, this.numbers.length + 1);
+        },
+        removeItem(index) {
+            this.numbers.splice(index, 1);
+        },
+        beforeEnter(el) {
+            console.log("beforeEnter");
+            this.elementWidth = 100;
+            el.style.width = this.elementWidth + "px";
+        },
+        enter(el, done) {
+            console.log("enter");
+            let round = 1;
+            const interval = setInterval(() => {
+                el.style.width = this.elementWidth + round * 10 + "px";
+                round++;
+                if (round > 20) {
+                    clearInterval(interval);
+                    done();
+                }
+            }, 20);
+        },
+        afterEnter(el) {
+            console.log("afterEnter");
+        },
+        enterCancelled(el) {
+            console.log("enterCancelled");
+        },
+        beforeLeave(el) {
+            console.log("beforeLeave");
+            this.elementWidth = 300;
+            el.style.width = this.elementWidth + "px";
+        },
+        leave(el, done) {
+            console.log("leave");
+            let round = 1;
+            const interval = setInterval(() => {
+                el.style.width = this.elementWidth - round * 10 + "px";
+                round++;
+                if (round > 20) {
+                    clearInterval(interval);
+                    done();
+                }
+            }, 20);
+        },
+        afterLeave(el) {
+            console.log("afterLeave");
+        },
+        leaveCancelled(el) {
+            console.log("leaveCancelled");
+        }
+    },
+    components: {
+        DangerAlert,
+        SuccessAlert
+    }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.fade-enter {
+    opacity: 0;
+}
+.fade-enter-active {
+    transition: opacity 1s;
+}
+.fade-leave {
+    /*opacity: 1;*/
+}
+.fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+}
+
+.slide-enter {
+    opacity: 0;
+}
+.slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity 1s;
+}
+.slide-leave {
+}
+.slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 1s;
+    opacity: 0;
+}
+
+.slide-move {
+    transition: transform 1s;
+}
+@keyframes slide-in {
+    from {
+        transform: translate(100px, 10px);
+    }
+    to {
+        transform: translate(0, 0);
+    }
+}
+@keyframes slide-out {
+    from {
+        transform: translate(0, 0);
+    }
+    to {
+        transform: translate(-100px, -10px);
+    }
 }
 </style>
